1a68f4d59411c567ade5db9f56ec75bd52926456
sharath G <150205015@gmail.com>
2017-01-24 00:10:20 -0600

    added changes

--- add.java
+++ add.java
@@ -1,3 +1,7 @@
 line 3 added
 new line on 01/22/17
+<<<<<<< Updated upstream
 new line added 01/22
+=======
+line4
+>>>>>>> Stashed changes
--- sub.java
+++ sub.java
@@ -1,4 +1,8 @@
 line 1 in sub.java
 line 2
 line 3
+<<<<<<< Updated upstream
 new line 4
+=======
+line 4 in sub
+>>>>>>> Stashed changes
